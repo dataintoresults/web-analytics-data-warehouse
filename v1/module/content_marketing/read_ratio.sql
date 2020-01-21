9c47bf4cd7f32b67cf2ea004c05204df5f9eebac
Sebastien <sderivaux@gmail.com>
2020-01-21 22:41:00 +0100

    Add read_ratio table and init.conf

---
+++ v1/module/content_marketing/read_ratio.sql
@@ -0,0 +1,38 @@
+-- Cleaning the pages in events to avoid duplicates
+with clean_events as (
+  select
+    regexp_replace(page, '[?&].*$', '') as clean_page, *
+  from google_analytics.events 
+),
+-- Cleaning the pages in sessions to avoid duplicates
+clean_sessions as (
+  select regexp_replace(page, '[?&].*$', '') as clean_page, *
+  from google_analytics.sessions 
+),
+-- For each page, how many people read more than 75% ?
+-- The Scroll event was implemented on 2020-01-15 so skip 
+-- everything before, it is not relevant
+end_of_page_events as (
+  select clean_page as page, sum(unique_events) as finish_count
+  from clean_events
+  where label = '75%'
+    and category = 'Scroll'
+    and date > '20200115'
+  group by 1
+),
+-- For each page track the number of unique pageviews
+-- We also just keep view after the implementation date of 
+-- scroll tracking.
+start_of_page_events as (
+  select clean_page as page, sum(unique_pageviews) as start_count
+  from clean_sessions
+  where date > '20200115'
+  group by 1
+)
+-- Blend those
+select page, start_count, finish_count, 
+  coalesce(finish_count, 0)*1.0/start_count as read_ratio
+from start_of_page_events 
+-- It's a left join because some articles are started
+-- but were never read up to the 75% mark.
+left join end_of_page_events using (page)
--- v1/store/google_sheets/users_country.sql
+++ v1/store/google_sheets/users_country.sql
@@ -1,21 +1,26 @@
+-- Aggregate raw data per month and country
 with users_country as (
   select to_char(to_date(date, 'YYYYMMDD'), 'YYYY-MM') as period,
     country, sum(users) as users
   from google_analytics.sessions
+  -- Take only the last 13 months (not the current one)
   where to_char(to_date(date, 'YYYYMMDD'), 'YYYY-MM') < to_char(current_date, 'YYYY-MM')
     and to_char(to_date(date, 'YYYYMMDD'), 'YYYY-MM') >= to_char(current_date - interval'13 months', 'YYYY-MM')
   group by 1, 2
 ),
+-- Order the countries by number of users in the previous month
 top_countries as (
   select country, row_number() over (order by users desc) as rk
   from users_country
   where period = to_char(current_date - interval'1 month', 'YYYY-MM')
 ),
+-- Select the best performing countries
 top_countries_select as (
   select *
   from top_countries
   where rk <= 5
 ),
+-- keep only the best 5 country and affect the rest to Other
 reduce_countries as (
   select period,
     coalesce(top_countries_select.country, 'Other') as country, 
@@ -24,6 +29,7 @@ reduce_countries as (
   left join top_countries_select using (country) 
   group by 1, 2
 )
+-- Finish with some ordering
 select *
 from reduce_countries
 order by 1 desc, 3 desc
